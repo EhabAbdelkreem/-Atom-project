cadfafa0fafb8aee49736e848338dee9bda63dd5
EhabAbdelkreem <108158201+EhabAbdelkreem@users.noreply.github.com>
2022-07-04 22:48:16 +0200

    gggggg
    commplete

--- TomProject/PL/ExportRecipts_Form.cs
+++ TomProject/PL/ExportRecipts_Form.cs
@@ -3,26 +3,293 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using TomProject.Context;
+using TomProject.Entities;
 
 namespace TomProject
 {
+
     public partial class ExportRecipts_Form : DevExpress.XtraEditors.XtraForm
     {
+        Garlic db = new Garlic();
         public ExportRecipts_Form()
         {
             InitializeComponent();
         }
-        Garlic db = new Garlic();
+
         private void ExportRecipts_Form_Load(object sender, EventArgs e)
         {
-            var result= db.Suppliers.Select(er => er).ToList();
-            gridControl1.DataSource = result;
+            var custom = db.Customers.Select(ee => new { ID = ee.ID, Name = ee.Name }).ToList();
+            LueItem_name.Text = db.Suppliers.Select(a => a.Name).FirstOrDefault();//to select first elment in form load
+            LueItem_name.Properties.DataSource = custom;
+            LueItem_name.Properties.DisplayMember = "Name";
+            LueItem_name.Properties.ValueMember = "ID";
+           
+            LueItem_name.Properties.PopulateColumns();
+            LueItem_name.Properties.Columns[0].Visible = false;
+
+
+
+
+            gridControl1.DataSource = db.ExportRecipets.Select(r => new { r.ID, Name = r.Customer.Name, r.Date, r.FillQuantity, r.Paid, r.Total, r.Remaining, r.Size, r.Notes, r.Price, r.NumberOfUnit, r.UnitWeight, r.TotalWeight }).ToList();
+
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+
+            if(comboBoxEdit_typofproduct.Text == " " ||
+             textBox_fillquantity.Text == " " ||
+            comboBoxEdit_size.Text == " " ||
+            textEdit_numofunit.Text == " " ||
+             textEdit_paid.Text ==  " " ||
+               LueItem_name.Text==" " ||
+               
+               richTextBox1.Text==" "||
+               textEdit1.Text==" "||
+               textEdit_weighttotal.Text==" "||
+             textEdit_total.Text == " " ||
+             textEdit_wightunit.Text == " " ||
+             textEdit_remaind.Text == " " )
+
+
+
+            {
+                XtraMessageBox.Show("Please Complete Data ", "Warnning", MessageBoxButtons.OK);
+
+            }
+            else
+            {
+                ExportRecipet ex = new ExportRecipet();
+                ex.Date = dateTimePicker1.Value;
+                ex.Notes = richTextBox1.Text;
+                ex.Type = comboBoxEdit_typofproduct.Text;
+                ex.Size = int.Parse(comboBoxEdit_size.Text);
+                ex.FillQuantity = int.Parse(textBox_fillquantity.Text);
+                ex.NumberOfUnit = int.Parse(textEdit_numofunit.Text);
+                ex.UnitWeight = float.Parse(textEdit_wightunit.Text);
+                ex.TotalWeight = float.Parse(textEdit_weighttotal.Text);
+                ex.Price = decimal.Parse(textEdit1.Text);
+                ex.Total = decimal.Parse(textEdit_total.Text);
+                ex.Paid = decimal.Parse(textEdit_paid.Text);
+                ex.Remaining = decimal.Parse(textEdit_remaind.Text);
+
+
+                ex.Customer = db.Customers.FirstOrDefault(d => d.ID == (int)LueItem_name.EditValue);
+                ex.Customer.account += decimal.Parse(textEdit_remaind.Text);
+
+
+
+
+                db.ExportRecipets.Add(ex);
+
+                db.SaveChanges();
+                
+                XtraMessageBox.Show("Added Succuessfuly ", "Successed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                     this.ExportRecipts_Form_Load(sender, e);
+                  textEdit_wightunit.Text = textEdit_weighttotal.Text =
+                    textBox_fillquantity.Text = textEdit_numofunit.Text = richTextBox1.Text = 
+                comboBoxEdit_size.Text = comboBoxEdit_typofproduct.Text  = dateTimePicker1.Text = LueItem_name.Text= "";
+
+                textEdit_total.Text = textEdit_paid.Text = textEdit1.Text= textEdit_remaind.Text = "0";
+
+
+            }
+
+
+
+
+        }
+
+        private void comboBox_name_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+
+            if (comboBoxEdit_typofproduct.Text == "" ||
+             textBox_fillquantity.Text == "" ||
+            comboBoxEdit_size.Text == "" ||
+            textEdit_numofunit.Text == "" ||
+             textEdit_paid.Text == "" ||
+               LueItem_name.Text == "" ||
+               dateTimePicker1.Text == "" ||
+               richTextBox1.Text == "" ||
+               textEdit1.Text ==""||
+               textEdit_weighttotal.Text == "" ||
+             textEdit_total.Text == "" ||
+             textEdit_wightunit.Text == "" ||
+             textEdit_remaind.Text == "")
+
+
+
+
+            {
+                XtraMessageBox.Show("Please Complete Data ", "Warnning", MessageBoxButtons.OK);
+
+            }
+            else
+            {
+                ExportRecipet ex = new ExportRecipet();
+                ex.Date = dateTimePicker1.Value;
+                ex.Notes = richTextBox1.Text;
+                ex.Type = comboBoxEdit_typofproduct.Text;
+                ex.Size = int.Parse(comboBoxEdit_size.Text);
+                ex.FillQuantity = int.Parse(textBox_fillquantity.Text);
+                ex.NumberOfUnit = int.Parse(textEdit_numofunit.Text);
+                ex.UnitWeight = float.Parse(textEdit_wightunit.Text);
+                ex.TotalWeight = float.Parse(textEdit_weighttotal.Text);
+                ex.Price = decimal.Parse(textEdit1.Text);
+                ex.Total = decimal.Parse(textEdit_total.Text);
+                ex.Paid = decimal.Parse(textEdit_paid.Text);
+                ex.Remaining = decimal.Parse(textEdit_remaind.Text);
+                
+                ex.Customer = db.Customers.FirstOrDefault(d => d.ID == (int)LueItem_name.EditValue);
+                int id = (int)gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "ID");
+                ex.ID = id;
+
+
+
+
+
+                decimal Old_Reminaing = (decimal)gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "Remaining");
+                ex.Customer.account += ex.Remaining - Old_Reminaing;
+
+
+                db.Entry(ex).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+
+
+               
+                gridControl1.DataSource = db.YeildRecipts.Select(r => new { r.ID, SName = r.Supplier.Name, Fdan = r.AcrePrice, r.CaratPrice, r.Paid, r.Total, r.Remaining, r.Date, r.Notes, Price = r.Price }).ToList();
+
+                
+                XtraMessageBox.Show("Updated Succuessfuly ", "Successed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                 textEdit_wightunit.Text = textEdit_weighttotal.Text =textBox_fillquantity.Text = textEdit_numofunit.Text = richTextBox1.Text= 
+                     
+                 comboBoxEdit_size.Text = comboBoxEdit_typofproduct.Text = dateTimePicker1.Text = LueItem_name.Text = "";
+
+                textEdit_remaind.Text = textEdit_paid.Text = textEdit_total.Text = "0";
+            }
+
+        }
+
+
+
+
+        private void lookUpEdit_typeofproduct_EditValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void simpleButton3_Click(object sender, EventArgs e)
+        {
+            
+            
+                if (gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "Name") != null)
+                {
+                    int id = (int)gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "ID");
+
+                    var receipt = db.ExportRecipets.Where(w => w.ID == id).Include(ee => ee.Customer).FirstOrDefault();
+                    if (XtraMessageBox.Show($"Do you want to delete Receipt number {id}?", "Confirmation", MessageBoxButtons.YesNo) != DialogResult.No)
+                    {
+                        
+                        receipt.Customer.account -= receipt.Remaining;
+                        
+                        db.ExportRecipets.Remove(receipt);
+                        db.SaveChanges();
+                        XtraMessageBox.Show("Removed Succuessfuly ", "Successed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    }
+                }
+
+
+                
+                gridControl1.DataSource = db.ExportRecipets.Select(r => new { r.ID, Name = r.Customer.Name, r.Date, r.FillQuantity, r.Paid, r.Total, r.Remaining, r.Size, r.Notes, r.Price, r.NumberOfUnit, r.UnitWeight, r.TotalWeight }).ToList();
+
+            textEdit_wightunit.Text = textEdit_weighttotal.Text = textBox_fillquantity.Text = textEdit_numofunit.Text = richTextBox1.Text =
+
+            comboBoxEdit_size.Text = comboBoxEdit_typofproduct.Text = dateTimePicker1.Text = LueItem_name.Text = "";
+
+            textEdit_remaind.Text = textEdit_paid.Text = textEdit_total.Text = "0";
+        }
+
+        private void gridView1_RowClick(object sender, DevExpress.XtraGrid.Views.Grid.RowClickEventArgs e)
+        {
+            gridView1_RowClick(sender, e, textEdit_numofunit.Text);
+        }
+
+        private void gridView1_RowClick(object sender, DevExpress.XtraGrid.Views.Grid.RowClickEventArgs e, string text)
+        {
+            if (gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "Name") != null)
+            {
+
+                
+                richTextBox1.Text = gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "Notes").ToString();
+                DateTime date = (DateTime)gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "Date");
+
+                dateTimePicker1.Text = date.ToShortDateString();
+                LueItem_name.Text = gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "Name").ToString();
+                comboBoxEdit_typofproduct.Text = gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "Type").ToString();
+                textBox_fillquantity.Text = gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "FillQuantity").ToString();
+                comboBoxEdit_size.Text = gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "Size").ToString();
+                textEdit_numofunit.Text = gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "NumberOfUnit").ToString();
+                textEdit_paid.Text = gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "Paid").ToString();
+                textEdit_remaind.Text = gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "Remaining").ToString();
+                textEdit_total.Text = gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "Total").ToString();
+                textEdit_weighttotal.Text = gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "TotalWeight").ToString();
+                textEdit_wightunit.Text = gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "UnitWeight").ToString();
+                textEdit1.Text = gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "Price").ToString();
+
+
+            }
+        }
+
+        private void textEdit1_EditValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textEdit_total_TextChanged(object sender, EventArgs e)
+        {
+            textEdit_remaind.Text = (decimal.Parse(textEdit_total.Text) - decimal.Parse(textEdit_paid.Text)).ToString();
+        }
+
+        private void textEdit_paid_TextChanged(object sender, EventArgs e)
+        {
+            textEdit_remaind.Text = (decimal.Parse(textEdit_total.Text) - decimal.Parse(textEdit_paid.Text)).ToString();
+        }
+
+        private void simpleButton4_Click(object sender, EventArgs e)
+        {
+            gridControl1.ShowPrintPreview();
+
+        }
+
+        private void comboBoxEdit1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ TomProject/PL/main_form.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace TomProject
+{
+    partial class main_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main_form));
+            this.ribbonPage1 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.ribbonPageGroup2 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.barButtonItem1 = new DevExpress.XtraBars.BarButtonItem();
+            this.barButtonItem2 = new DevExpress.XtraBars.BarButtonItem();
+            this.barButtonItem3 = new DevExpress.XtraBars.BarButtonItem();
+            this.barButtonItem4 = new DevExpress.XtraBars.BarButtonItem();
+            this.barButtonItem5 = new DevExpress.XtraBars.BarButtonItem();
+            this.barButtonItem6 = new DevExpress.XtraBars.BarButtonItem();
+            this.barButtonItem7 = new DevExpress.XtraBars.BarButtonItem();
+            this.ribbon = new DevExpress.XtraBars.Ribbon.RibbonControl();
+            this.barButtonItem8 = new DevExpress.XtraBars.BarButtonItem();
+            this.ribbonPage2 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.ribbonPageGroup3 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.ribbonPage3 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.ribbonPageGroup4 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.ribbonPage4 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.ribbonPageGroup5 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.ribbonPage5 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.ribbonPageGroup6 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.ribbonPage6 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.ribbonPageGroup7 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.ribbonPage7 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.ribbonPageGroup8 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.الصادرات = new DevExpress.XtraBars.BarButtonItem();
+            this.barAndDockingController1 = new DevExpress.XtraBars.BarAndDockingController(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ribbon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barAndDockingController1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ribbonPage1
+            // 
+            this.ribbonPage1.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ribbonPageGroup1,
+            this.ribbonPageGroup2});
+            this.ribbonPage1.Name = "ribbonPage1";
+            this.ribbonPage1.Text = "العملاء";
+            // 
+            // ribbonPageGroup2
+            // 
+            this.ribbonPageGroup2.ItemLinks.Add(this.barButtonItem8);
+            this.ribbonPageGroup2.Name = "ribbonPageGroup2";
+            this.ribbonPageGroup2.Text = "معاملات العملاء";
+            // 
+            // ribbonPageGroup1
+            // 
+            this.ribbonPageGroup1.ItemLinks.Add(this.barButtonItem3);
+            this.ribbonPageGroup1.Name = "ribbonPageGroup1";
+            this.ribbonPageGroup1.Text = "العملاء";
+            // 
+            // barButtonItem1
+            // 
+            this.barButtonItem1.Caption = "اضافة عميل";
+            this.barButtonItem1.Id = 1;
+            this.barButtonItem1.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("barButtonItem1.ImageOptions.SvgImage")));
+            this.barButtonItem1.Name = "barButtonItem1";
+            // 
+            // barButtonItem2
+            // 
+            this.barButtonItem2.Caption = "معاملات العملاء";
+            this.barButtonItem2.Id = 2;
+            this.barButtonItem2.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("barButtonItem2.ImageOptions.SvgImage")));
+            this.barButtonItem2.Name = "barButtonItem2";
+            // 
+            // barButtonItem3
+            // 
+            this.barButtonItem3.Caption = "العملاء";
+            this.barButtonItem3.Id = 3;
+            this.barButtonItem3.Name = "barButtonItem3";
+            this.barButtonItem3.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barButtonItem3_ItemClick);
+            // 
+            // barButtonItem4
+            // 
+            this.barButtonItem4.Caption = "barButtonItem4";
+            this.barButtonItem4.Id = 4;
+            this.barButtonItem4.Name = "barButtonItem4";
+            // 
+            // barButtonItem5
+            // 
+            this.barButtonItem5.Id = 5;
+            this.barButtonItem5.Name = "barButtonItem5";
+            // 
+            // barButtonItem6
+            // 
+            this.barButtonItem6.Caption = "barButtonItem6";
+            this.barButtonItem6.Id = 6;
+            this.barButtonItem6.Name = "barButtonItem6";
+            // 
+            // barButtonItem7
+            // 
+            this.barButtonItem7.Caption = "barButtonItem7";
+            this.barButtonItem7.Id = 7;
+            this.barButtonItem7.Name = "barButtonItem7";
+            // 
+            // ribbon
+            // 
+            this.ribbon.Controller = this.barAndDockingController1;
+            this.ribbon.EmptyAreaImageOptions.ImagePadding = new System.Windows.Forms.Padding(50, 43, 50, 43);
+            this.ribbon.ExpandCollapseItem.Id = 0;
+            this.ribbon.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.ribbon.SearchEditItem,
+            this.ribbon.ExpandCollapseItem,
+            this.barButtonItem4,
+            this.barButtonItem5,
+            this.barButtonItem6,
+            this.barButtonItem7,
+            this.barButtonItem1,
+            this.barButtonItem2,
+            this.barButtonItem3,
+            this.barButtonItem8,
+            this.الصادرات});
+            this.ribbon.Location = new System.Drawing.Point(0, 0);
+            this.ribbon.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ribbon.MaxItemId = 10;
+            this.ribbon.Name = "ribbon";
+            this.ribbon.OptionsMenuMinWidth = 550;
+            this.ribbon.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+            this.ribbonPage1,
+            this.ribbonPage2,
+            this.ribbonPage3,
+            this.ribbonPage4,
+            this.ribbonPage5,
+            this.ribbonPage6,
+            this.ribbonPage7});
+            this.ribbon.RibbonStyle = DevExpress.XtraBars.Ribbon.RibbonControlStyle.Office2007;
+            this.ribbon.Size = new System.Drawing.Size(865, 148);
+            // 
+            // barButtonItem8
+            // 
+            this.barButtonItem8.Caption = "معملات العملاء";
+            this.barButtonItem8.Id = 8;
+            this.barButtonItem8.Name = "barButtonItem8";
+            this.barButtonItem8.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barButtonItem8_ItemClick);
+            // 
+            // ribbonPage2
+            // 
+            this.ribbonPage2.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ribbonPageGroup3});
+            this.ribbonPage2.Name = "ribbonPage2";
+            this.ribbonPage2.Text = "الموردين";
+            // 
+            // ribbonPageGroup3
+            // 
+            this.ribbonPageGroup3.Name = "ribbonPageGroup3";
+            this.ribbonPageGroup3.Text = "ribbonPageGroup3";
+            // 
+            // ribbonPage3
+            // 
+            this.ribbonPage3.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ribbonPageGroup4});
+            this.ribbonPage3.Name = "ribbonPage3";
+            this.ribbonPage3.Text = "الواردات";
+            // 
+            // ribbonPageGroup4
+            // 
+            this.ribbonPageGroup4.Name = "ribbonPageGroup4";
+            this.ribbonPageGroup4.Text = "ribbonPageGroup4";
+            // 
+            // ribbonPage4
+            // 
+            this.ribbonPage4.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ribbonPageGroup5});
+            this.ribbonPage4.Name = "ribbonPage4";
+            this.ribbonPage4.Text = "الصادرات";
+            // 
+            // ribbonPageGroup5
+            // 
+            this.ribbonPageGroup5.ItemLinks.Add(this.الصادرات);
+            this.ribbonPageGroup5.Name = "ribbonPageGroup5";
+            this.ribbonPageGroup5.Text = "الصادرات";
+            // 
+            // ribbonPage5
+            // 
+            this.ribbonPage5.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ribbonPageGroup6});
+            this.ribbonPage5.Name = "ribbonPage5";
+            this.ribbonPage5.Text = "التعبئه";
+            // 
+            // ribbonPageGroup6
+            // 
+            this.ribbonPageGroup6.Name = "ribbonPageGroup6";
+            this.ribbonPageGroup6.Text = "ribbonPageGroup6";
+            // 
+            // ribbonPage6
+            // 
+            this.ribbonPage6.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ribbonPageGroup7});
+            this.ribbonPage6.Name = "ribbonPage6";
+            this.ribbonPage6.Text = "الخزنه";
+            // 
+            // ribbonPageGroup7
+            // 
+            this.ribbonPageGroup7.Name = "ribbonPageGroup7";
+            this.ribbonPageGroup7.Text = "ribbonPageGroup7";
+            // 
+            // ribbonPage7
+            // 
+            this.ribbonPage7.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ribbonPageGroup8});
+            this.ribbonPage7.Name = "ribbonPage7";
+            this.ribbonPage7.Text = "التقارير";
+            // 
+            // ribbonPageGroup8
+            // 
+            this.ribbonPageGroup8.Name = "ribbonPageGroup8";
+            this.ribbonPageGroup8.Text = "ribbonPageGroup8";
+            // 
+            // الصادرات
+            // 
+            this.الصادرات.Caption = "الصادرات";
+            this.الصادرات.Id = 9;
+            this.الصادرات.Name = "الصادرات";
+            this.الصادرات.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.الصادرات_ItemClick);
+            // 
+            // barAndDockingController1
+            // 
+            this.barAndDockingController1.LookAndFeel.SkinName = "Office 2010 Blue";
+            this.barAndDockingController1.LookAndFeel.UseDefaultLookAndFeel = false;
+            this.barAndDockingController1.PropertiesBar.AllowLinkLighting = false;
+            // 
+            // main_form
+            // 
+            this.Appearance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.Appearance.Options.UseFont = true;
+            this.Appearance.Options.UseForeColor = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(865, 683);
+            this.Controls.Add(this.ribbon);
+            this.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "main_form";
+            this.Ribbon = this.ribbon;
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.Text = "الصفحة الرئيسيه";
+            this.Load += new System.EventHandler(this.main_form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ribbon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barAndDockingController1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage1;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup1;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem3;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup2;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem8;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem1;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem2;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem4;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem5;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem6;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem7;
+        private DevExpress.XtraBars.Ribbon.RibbonControl ribbon;
+        private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage2;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup3;
+        private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage3;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup4;
+        private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage4;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup5;
+        private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage5;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup6;
+        private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage6;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup7;
+        private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage7;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup8;
+        private DevExpress.XtraBars.BarButtonItem الصادرات;
+        private DevExpress.XtraBars.BarAndDockingController barAndDockingController1;
+    }
+}
